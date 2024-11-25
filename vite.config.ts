68ce6ae6611f4ca3bc8b4ffb7d04c3d42d71c42f
Potato0706 <potatochips0706@gmail.com>
2024-11-26 06:34:25 +0800

    Removing custom CSS tree resolver and adding external dependency for SQLite

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		{
-			name: 'custom-css-tree-resolver',
-			resolveId(id) {
-				if (id === 'css-tree') {
-					return require.resolve('./node_modules/css-tree/dist/csstree.esm.js');
-				}
-			}
+	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			external: ['bun:sqlite']
 		}
-	]
+	}
 });
