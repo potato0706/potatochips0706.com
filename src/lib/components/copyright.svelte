a15872682f74f6e55c34acf78b987bdca48fd4ba
Potato0706 <potatochips0706@gmail.com>
2024-11-26 06:34:42 +0800

    Update copyright component to include project open-source information and GPL-3.0 license

--- src/lib/components/copyright.svelte
+++ src/lib/components/copyright.svelte
@@ -1,9 +1,17 @@
-<p class="copyright sub-text">&COPY; 2024 <a href=".">Potato_Chips0706</a></p>
+<div class="copyright sub-text">
+	<p>
+		This project is open-sourced on <a href="https://github.com/potato0706/potatochips0706.com"
+			>GitHub</a
+		>!
+	</p>
+	<p>&COPY; 2024 <a href=".">Potato_Chips0706</a> under GPL-3.0 license</p>
+</div>
 
 <style>
 	.copyright {
 		position: fixed;
 		bottom: 0.25em;
 		right: 1rem;
+		text-align: right;
 	}
 </style>
