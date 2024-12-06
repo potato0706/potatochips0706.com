aa82a1d3e246e2365592f25d5fd868cd8b9bf6c0
Potato0706 <potatochips0706@gmail.com>
2024-12-07 00:40:18 +0800

    Simplify console log message in server startup

--- server.ts
+++ server.ts
@@ -6,5 +6,5 @@ const app = express();
 app.use(handler);
 
 app.listen(process.env.PORT ?? 3000, () => {
-	console.log(`App potatochips0706.com is running on PORT ${process.env.PORT ?? 3000}`);
+	console.log(`App is running on PORT ${process.env.PORT ?? 3000}`);
 });
