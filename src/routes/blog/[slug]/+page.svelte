c768a264853cbb21ea6af9e110b68c22d9cb702f
Potato0706 <potatochips0706@gmail.com>
2024-12-08 13:05:10 +0800

    Refactor blog post page to use updated data structure for title, date, and content

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -6,12 +6,12 @@
 </script>
 
 <article>
-	<h1>{data.post.title}</h1>
-	<time datetime={data.post.date}>
-		{format(new Date(data.post.date), 'MMMM d, yyyy')}
+	<h1>{data.title}</h1>
+	<time datetime={data.date}>
+		{format(new Date(data.date), 'MMMM d, yyyy')}
 	</time>
 
-	{@html data.post.content}
+	{@html data.content}
 </article>
 
 <a href="/blog">Back to blog</a>
